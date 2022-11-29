ce95190fd62a45488009351da70ab344b84b2fe7
DiigoTardeo <rodrigo.barbosa@sptech.school>
2022-11-29 01:36:14 -0300

    modificação nos arquivos, implementação do mural de avisos, conserto no banco de dados e seus campos, modificações nos arquivos src, criação da tela com os campos de cruzadinha e começo da tela de rank. Alem de modificação em uma parte do css da tela de Onis

--- web-data-viz-main/site/public/js/funcoes.js
+++ web-data-viz-main/site/public/js/funcoes.js
@@ -5,11 +5,11 @@ function validarSessao() {
     var email = sessionStorage.EMAIL_USUARIO;
     var nome = sessionStorage.NOME_USUARIO;
 
-    var b_usuario = document.getElementById("b_usuario");
+    var usuario = document.getElementById("usuario");
 
     if (email != null && nome != null) {
         // window.alert(`Seja bem-vindo, ${nome}!`);
-        b_usuario.innerHTML = nome;
+        usuario.innerHTML = nome;
 
         // finalizarAguardar();
     } else {
@@ -52,3 +52,54 @@ function fecharModal() {
     divModal.style.display = "none";
 }
 
+
+    /* ABRIR E FECHAR O HEADER*/
+    function abrirAba(){
+        btn_abreAba.style.display= "none";
+        aba_left.style.width="300px";
+        btn_fechaAba.style.display= "flex";
+        navegar_header.style.margin= "0px 0px 0px 0px";
+        container_dados.style.margin= "20px 20px 0px 325px";
+        container_missoes.style.margin= "20px 20px 0px 325px";
+        container_quiz.style.margin= "20px 20px 0px 325px";
+        btn_sair.style.display="flex";
+    }
+
+    function fecharAba(){
+        btn_abreAba.style.display= "block";
+        aba_left.style.width="95px";
+        btn_fechaAba.style.display= "none";
+        navegar_header.style.margin=" 0px 0px 0px -100vw";
+        container_dados.style.margin= "20px 20px 0px 120px";
+        container_missoes.style.margin= "20px 20px 0px 120px";
+        container_quiz.style.margin= "20px 20px 0px 120px";
+    }
+
+    // /* BOTÕES DO HEADER  */ 
+    function meus_dados(){
+        container_dados.style.display = "flex";
+        container_missoes.style.display = "none";
+        container_voto.style.display = "none";
+        // container_sugestao.style.display = "none";
+    }
+
+    function missoes(){
+        container_dados.style.display = "none";
+        container_missoes.style.display = "flex";
+        container_voto.style.display = "none";
+        // container_sugestao.style.display = "none";
+    }
+
+    function voto(){
+        container_dados.style.display = "none";
+        container_missoes.style.display = "none";
+        container_voto.style.display = "flex";
+        // container_sugestao.style.display = "none";
+    }
+
+    function sugestao(){
+        container_dados.style.display = "none";
+        container_missoes.style.display = "none";
+        container_voto.style.display = "none";
+        // container_sugestao.style.display = "flex";
+    }
